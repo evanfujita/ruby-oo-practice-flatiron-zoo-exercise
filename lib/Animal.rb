09abf73820ad8e391d6dac3c2096d134f4440826
Evan <evanfujita@gmail.com>
2020-12-17 12:20:07 -0600

    done

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,23 @@
 class Animal
 
+    attr_reader :species, :nickname
+    attr_accessor :weight, :zoo
+
+    @@all = []
+
+    def initialize(species, weight, nickname)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(type)
+        self.all.select {|animal| animal.species == type}
+    end
+
 end
--- run.rb
+++ run.rb
@@ -3,7 +3,22 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+brookfield = Zoo.new("Brookfield", "Chicago")
+lp = Zoo.new("Lincoln Park", "Chicago")
+brooklyn = Zoo.new("Brooklyn", "New York")
+
+lp.new_animal("Cat", 7, "Winston")
+lp.new_animal("Cat", 8, "Mouse")
+lp.new_animal("Cat", 2, "Eliot")
+lp.new_animal("Dog", 12, "Scout")
+lp.new_animal("Dog", 11, "Maxwell")
+lp.new_animal("Dog", 4, "Sonny")
+brookfield.new_animal("Rat", 7, "Ratatat")
+brookfield.new_animal("Rat", 8, "Apple")
+brookfield.new_animal("Rat", 2, "John")
+brookfield.new_animal("Giraffe", 12, "Mark")
+brookfield.new_animal("Hippo", 11, "Alf")
+brookfield.new_animal("Bat", 4, "Elf")
 
 
 binding.pry
